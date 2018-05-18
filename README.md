fc8a4d12d1ec30db5b0d92c5bad27ee14a12662c
André Mendes <andremendes@MacBook-Pro-de-SUPPORT.local>
2018-05-18 13:04:07 -0300

    removing subfolder

---
+++ README.md
@@ -0,0 +1,40 @@
+# Visual Regression with BackstopJS
+
+A small example of visual regression testing with [BackstopJS](https://github.com/garris/BackstopJS) in a pokedex application.
+
+- A pokedex web app using AngularJS and [Pokeapi.co](http://pokeapi.co/).
+
+- This app was based in <https://github.com/alvarowolfx/ng-pokedex/> 
+
+# How to run the application
+
+Install required npm packages, run this command on project folder :
+
+  ```
+  npm install
+  ```
+
+Start livereload server
+
+  ```
+  npm start
+  ```
+
+
+# How to run tests
+
+Inside `visual-regression` folder :
+
+```
+npm install
+  ```
+
+```
+npm test
+  ``` 
+
+To promote images to baseline:
+
+```
+npm approve
+  ``` 
---
+++ visual-regression/backstop_data/html_report/config.js
@@ -0,0 +1,94 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/pokedex_test_Pokedex_0_bulbasaur_0_tablet.png",
+        "test": "../bitmaps_test/20180518-130227/pokedex_test_Pokedex_0_bulbasaur_0_tablet.png",
+        "selector": "#bulbasaur",
+        "fileName": "pokedex_test_Pokedex_0_bulbasaur_0_tablet.png",
+        "label": "Pokedex",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:3000/#/pokemons",
+        "referenceUrl": "",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/pokedex_test_Pokedex_1_rattata_0_tablet.png",
+        "test": "../bitmaps_test/20180518-130227/pokedex_test_Pokedex_1_rattata_0_tablet.png",
+        "selector": "#rattata",
+        "fileName": "pokedex_test_Pokedex_1_rattata_0_tablet.png",
+        "label": "Pokedex",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:3000/#/pokemons",
+        "referenceUrl": "",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/pokedex_test_Pokedex_2_gengar_0_tablet.png",
+        "test": "../bitmaps_test/20180518-130227/pokedex_test_Pokedex_2_gengar_0_tablet.png",
+        "selector": "#gengar",
+        "fileName": "pokedex_test_Pokedex_2_gengar_0_tablet.png",
+        "label": "Pokedex",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:3000/#/pokemons",
+        "referenceUrl": "",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/pokedex_test_Pokedex_3_torchic_0_tablet.png",
+        "test": "../bitmaps_test/20180518-130227/pokedex_test_Pokedex_3_torchic_0_tablet.png",
+        "selector": "#torchic",
+        "fileName": "pokedex_test_Pokedex_3_torchic_0_tablet.png",
+        "label": "Pokedex",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:3000/#/pokemons",
+        "referenceUrl": "",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "pokedex_test"
+});
